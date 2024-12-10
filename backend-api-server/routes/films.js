5c8a41428e99bb165d3754aa5b8fb5dd56ea1a1a
Efestus31 <biagiohd@gmail.com>
2024-12-10 14:26:10 +0100

    add show query

--- backend-api-server/routes/films.js
+++ backend-api-server/routes/films.js
@@ -4,10 +4,37 @@ const connection = require('../db/connection')
 
 //Get all Films
 router.get('/', (req, res) => {
-    res.json({
-        films: 'all films here'
-    })
+    const sql = `SELECT * FROM movies`;
+
+    connection.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ err: err })
+
+        res.json({
+            movies: results,
+            count: results.length
+        })
+    });
 });
+
+router.get('/:id', (req, res) => {
+
+    const id = req.params.id
+    const sql = `SELECT * FROM movies WHERE id = ?`;
+
+    //get movie by given id
+    connection.query(sql, [id], (err, results) => {
+        if (err) return res.status(500).json({ err: err })
+        console.log(results);
+
+        res.json({
+            movies: `returning the movie with an id of ${id}`
+        })
+
+    });
+
+
+});
+
 
 
 module.exports = router
