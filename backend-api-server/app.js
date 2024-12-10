0e0fba59b1b7c386fa9ccddfb8b7a9b50f91265c
Efestus31 <biagiohd@gmail.com>
2024-12-10 15:13:12 +0100

    update app.js

--- backend-api-server/app.js
+++ backend-api-server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const server = express()
 const FilmsRouter = require('./routes/films')
 const notFound = require('./middlewares/NotFound')
+const ServerErrorsHandler = require('./middlewares/ServerErrorsHandler')
 
 const PORT = process.env.PORT || 3001;
 const HOST = process.env.HOST || "localhost";
@@ -15,6 +16,10 @@ server.get('/', (req, res) => {
 })
 //middleware to handle 404 error with a catch all route
 server.use(notFound)
+
+//middleware to handle 500 errors
+server.use(ServerErrorsHandler)
+
 
 server.listen(PORT, () => {
     console.log(`Server is running on ${HOST}:${PORT}`);
